fd7eae2017f4c67d966a6bd0a2d7c3d65f375396
Marco Steiner <64867140+marco-steiner@users.noreply.github.com>
2023-02-03 11:50:23 +0100

    Add files via upload

--- pdf.php
+++ pdf.php
@@ -2,6 +2,9 @@
     $datum = time(); // Date
     $kw = date("W", $datum); // KW
     $year = date("Y"); // Year
+    $kw = (int)$kw; // Integer remove first 0
+
+    //$kw = ++$kw; // 2023 eine KW verschoben
 
     if(date('D') == 'Mon' || date('D') == 'Tue' || date('D') == 'Wed') { 
         $kw = --$kw; // KW minus 1
@@ -12,7 +15,10 @@
         echo '<!--https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf-->';
         echo '<!-- Day: '.date('D').'--><br>';
         $file = file_get_contents('https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf');
-        file_put_contents('pdf/'.$kw.'.pdf', $file);
+        if (!file_exists('pdf/'.$year)) {
+            mkdir('pdf/'.$year, 0777, true);
+        }
+        file_put_contents('pdf/'.$year.'/'.$kw.'.pdf', $file);
     }
     else {
         // it's not enabled, so do something else

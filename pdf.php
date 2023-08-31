47786380ff118111a606c8b355a33ea44bfea7c4
Marco Steiner <64867140+marco-steiner@users.noreply.github.com>
2023-08-31 12:57:42 +0200

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -12,9 +12,11 @@
 
     if( ini_get('allow_url_fopen') ) {
         // it's enabled, so do something
-        echo '<!--https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf-->';
+        //echo '<!--https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf-->';
+        echo '<!--https://epaper.wittich.de/frontend/mvc/catalog/by-name/pdf/2421/2421_'.$kw.'_'.$year.'-->';
         echo '<!-- Day: '.date('D').'--><br>';
-        $file = file_get_contents('https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf');
+        //$file = file_get_contents('https://archiv.wittich.de/epapers/pdf/2421/'.$year.'/'.$kw.'.pdf');
+        $file = file_get_contents('https://epaper.wittich.de/frontend/mvc/catalog/by-name/pdf/2421/2421_'.$kw.'_'.$year);
         if (!file_exists('pdf/'.$year)) {
             mkdir('pdf/'.$year, 0777, true);
         }
@@ -23,4 +25,7 @@
     else {
         // it's not enabled, so do something else
     }
+
+    // https://epaper.wittich.de/frontend/mvc/catalog/by-name/2421/2421_35_2023
+    // https://epaper.wittich.de/frontend/mvc/catalog/by-name/pdf/2421/2421_35_2023
 ?>
